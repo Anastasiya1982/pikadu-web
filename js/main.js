c66989b3d2123f3f24905dd5d91345ad25b7c628
ACER <mahanasty@mail.ru>
2020-11-11 19:10:33 +0300

    autorized and registration

--- js/main.js
+++ js/main.js
@@ -2,12 +2,127 @@
 let menuToggle = document.querySelector('#menu-toggle');
 // Создаем переменную, в которую положим меню
 let menu = document.querySelector('.sidebar');
-// отслеживаем клик по кнопке меню и запускаем функцию 
+// отслеживаем клик по кнопке меню и запускаем функцию
 menuToggle.addEventListener('click', function (event) {
   // отменяем стандартное поведение ссылки
   event.preventDefault();
-  // вешаем класс на меню, когда кликнули по кнопке меню 
+  // вешаем класс на меню, когда кликнули по кнопке меню
   menu.classList.toggle('visible');
 })
 
+
+// создаем перменные для получения элементов блока логинизации
+
+const loginElem = document.querySelector('.login');
+const loginForm=document.querySelector('.login-form');
+const emailInput=document.querySelector('.login-email');
+const passwordInput=document.querySelector('.login-password');
+const loginSignup=document.querySelector('.login-signup');
+
+// получаем элементы блока юзер ( когда зарегистрировался пользователь и вошел)
+const userElem = document.querySelector('.user');
+const userNameElem = document.querySelector('.user-name');
+
+// создаем массив юзеров пока нет базы данных
+const listUsers=[
+  {
+    id:'01',
+    email:"mahanasty@mail.com",
+    password:'123456',
+    displayName:'NastyaJS',
+  },
+  {
+    id:'02',
+    email:"katerol@mail.com",
+    password:'789555',
+    displayName:'KillKate',
+  },
+  {
+    id:'03',
+    email:"maksLeskin@mail.com",
+    password:'112255',
+    displayName:'MaksJS',
+  }
+];
+
+
+const setUsers= {
+  user: null,
+  //добавляем методы
+  logIn(email, password,handler) {
+   const user=this.getUser(email);
+   if(user && user.password===password){
+     this.autorizedUser(user);
+     handler();
+   }else{
+     alert('Пользователь с такими данными не найден');
+   }
+  },
+  logOut() {
+    console.log('logOut')
+  },
+  signUp(email, password,handler) {
+    if (!this.getUser(email)) {
+      const user = {email, password, displayName: email}
+      listUsers.push(user);
+      this.autorizedUser(user);
+      handler();
+    } else {
+      alert("Пользователь с таким email уже зарегистрирован");
+    }
+  },
+  getUser(email) {
+    let user = null;
+    // получаем данные каждого объекта  и сверяем
+    //   for(let i = 0; i <listUsers.length; i++){
+    //     if(listUsers[i].email === email){
+    //       user =listUsers[i];
+    //       break;
+    //     }
+    //   }
+    //   return user;
+
+    // другой способ с помощью findб упрощаем код
+    return listUsers.find((item)=>item.email===email)
+  },
+  autorizedUser(user){
+    this.user=user;
+  }
+};
+// функция переключения авторизации ( меняем login  на user )
+const toggleAuthDom =()=>{
+  const user = setUsers.user;
+  console.log(user);
+
+  if(user){
+    loginElem.style.display = 'none';
+    userElem.style.display = '';
+    userNameElem.textContent = user.displayName;
+  } else {
+    loginElem.style.display = '';
+    userElem.style.display = 'none';
+  }
+};
+
+
+loginForm.addEventListener('submit',(event)=>{
+  event.preventDefault();
+  const emailValue=emailInput.value;
+  const passwordValue=passwordInput.value;
+  setUsers.logIn(emailValue, passwordValue, toggleAuthDom);
+
+
+})
+
+
+loginSignup.addEventListener('click',(event)=>{
+  event.preventDefault();
+
+  const emailValue=emailInput.value;
+  const passwordValue=passwordInput.value;
+
+  setUsers.signUp(emailValue, passwordValue, toggleAuthDom);
+
+})
+toggleAuthDom();
 
